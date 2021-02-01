bc9c7135011b67f90857913ae57477d250a05b61
ighodalo eagles william <eaglesdalo88@gmail.com>
2021-02-01 17:10:36 +0100

    create and delete comment

--- server/controller/post.js
+++ server/controller/post.js
@@ -60,3 +60,70 @@ exports.deletePostById = asyncHandler(async (req, res) => {
   await post.remove();
   res.json(post);
 });
+
+// route   Put /api/v1/post/likes/:id
+// @desc    put post by id
+// @access  Private
+exports.likePosts = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post.likes.filter((like) => like.user.toString() === req.user.id) > 0)
+    return res.status(400).json({ msg: "post already liked by this user" });
+  post.likes.unshift({ user: req.user.id });
+  await post.save();
+  res.json(post.likes);
+});
+// route   Put /api/v1/post/unlike/:id
+// @desc    put post by id
+// @access  Private
+exports.unlikePosts = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if ((post.likes.filter((like) => like.user.toString() === req.user.id) = 0))
+    return res.status(400).json({ msg: "post yet to be liked by this user" });
+  const removeIndex = post.likes
+    .map((like) => like.user.toString())
+    .indexOf(req.user.id);
+  post.likes.splice(removeIndex, 1);
+  await post.save();
+  res.json(post.likes);
+});
+
+// route   COMMENT /api/v1/post/comment/:id
+// @desc    comment on a post
+// @access  Private
+exports.createComment = asyncHandler(async (req, res) => {
+  await check("text", "text field cannot be empty").not().isEmpty().run(req);
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
+  const user = await User.findById(req.user.id).select("-password");
+  const post = await Post.findById(req.params.id);
+
+  const newComment = {
+    text: req.body.text,
+    name: user.name,
+    avatar: user.avatar,
+    user: req.user.id,
+  };
+  post.comments.unshift(newComment);
+  await post.save();
+  res.json(post.comments);
+});
+// route   DELETE /api/v1/post/comment/:id/:comment_id
+// @desc    delete comment on a post
+// @access  Private
+exports.deleteComment = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  //   pull out the comment
+  const comment = post.comments.find(
+    (comment) => comment.id === req.params.comment_id
+  );
+  if (!comment) return res.status(404).json({ msg: "comment does not exists" });
+  if (comment.user.toString() !== req.user.id)
+    return res.status(401).json({ msg: "user not authorized" });
+  const removeIndex = post.comments
+    .map((comment) => comment.id)
+    .indexOf(req.params.id);
+  post.comments.splice(removeIndex, 1);
+  await post.save();
+  res.json(post.comments);
+});
--- server/routes/post.js
+++ server/routes/post.js
@@ -4,6 +4,10 @@ const {
   getPost,
   getPostById,
   deletePostById,
+  likePosts,
+  unlikePosts,
+  createComment,
+  deleteComment,
 } = require("../controller/post");
 const { auth } = require("../middleware/authentication");
 
@@ -13,5 +17,9 @@ router
   .get("/me", auth, getUserPost)
   .get("/", auth, getPost)
   .get("/:id", auth, getPostById)
-  .delete("/:id", auth, deletePostById);
+  .delete("/:id", auth, deletePostById)
+  .put("/likes/:id", auth, likePosts)
+  .put("/unlikes/:id", auth, unlikePosts)
+  .put("/comment/:id", auth, createComment)
+  .delete("/comment/:id/:comment_id", auth, deleteComment);
 module.exports = router;
